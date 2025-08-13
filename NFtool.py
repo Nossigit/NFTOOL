c42ea9e1daf91a24a783ee9664d1025228affc31
Nossigit <nossitwitch@gmail.com>
2025-08-13 02:34:45 +0200

    Update NFtool.py

--- NFtool.py
+++ NFtool.py
@@ -20,7 +20,7 @@ from packaging import version
 
 
 # ====== APP / UPDATE SETTINGS ======
-VERSION = "0.4.3"               
+VERSION = "0.4.4"               
 GITHUB_OWNER = "Nossigit"
 GITHUB_REPO  = "NFTOOL"
 RELEASE_ASSET_NAME = "NFTOOL.exe"  #
